3a78a575bb96b4e91bce53bbe9d86e1302f644dc
Maurício Moreira Neto <maumneto@gmail.com>
2018-04-23 14:57:52 -0300

    new code

--- velocidade.R
+++ velocidade.R
@@ -1,12 +1,14 @@
 ## preparing the workspace for new simulations
 rm(list=ls(all=TRUE));
-
+# Using the dataset 
 source("C:\\Users\\Mauricio\\Downloads\\eHealthApproach-master\\eHealthData.R");
 
+# Assignment of data column values
 valueDataX <- HealthDataSerie(15);
 valueDataY <- HealthDataSerie(16);
 valueDataZ <- HealthDataSerie(17);
 
+# Variables
 X = valueDataX[1:200]
 Y = valueDataY[1:200]
 Z = valueDataZ[1:200]
@@ -15,13 +17,14 @@ velocityY = 0;
 velocityZ = 0;
 timeHorizon = 1:200;
 
-
+# Creating three vectors of zeros of X-,Y- and Z- axis
 for (a in 1:200) {
   velocityX[a] = 0;
   velocityY[a] = 0;
   velocityZ[a] = 0;
 }
 
+# Calculating the velocity of the each axis (sampling time = 0.001s)
 for(i in 2:length(X)){
   velocityX[i] = velocityX[i-1] + (X[i])*0.001;
   velocityCurentX = velocityX[i];
@@ -36,6 +39,12 @@ for(i in 2:length(X)){
   velocityBeforeZ = velocityZ[i-1];
 }
 
+# calculating the number of steps based on the 0.8m step
+numberOfStepsX = (velocityCurentX*length(X))/0.8
+numberOfStepsY = (velocityCurentY*length(Y))/0.8
+numberOfStepsZ = (velocityCurentZ*length(Z))/0.8
+
+#plot
 plot(timeHorizon, velocityX, col = "black")
 lines(timeHorizon,velocityY, col = "blue")
 lines(timeHorizon, velocityZ, col = "red")
