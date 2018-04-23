7877040631ebdea9c1f97522a58c1c52e3a38478
Maurício Moreira Neto <maumneto@gmail.com>
2018-04-23 14:25:46 -0300

    new

--- velocidade.R
+++ velocidade.R
@@ -10,20 +10,33 @@ valueDataZ <- HealthDataSerie(17);
 X = valueDataX[1:200]
 Y = valueDataY[1:200]
 Z = valueDataZ[1:200]
-vx = 0;
-
+velocityX = 0;
+velocityY = 0;
+velocityZ = 0;
 timeHorizon = 1:200;
 
+
 for (a in 1:200) {
-  vx[a] = 0;
+  velocityX[a] = 0;
+  velocityY[a] = 0;
+  velocityZ[a] = 0;
 }
 
 for(i in 2:length(X)){
-  #X[i] <- X;
-  vx[i] = vx[i-1] + (X[i])*0.001;
-  velocityR = vx[i];
-  newValueR = vx[i-1];
+  velocityX[i] = velocityX[i-1] + (X[i])*0.001;
+  velocityCurentX = velocityX[i];
+  velocityBeforeX = velocityX[i-1];
+  
+  velocityY[i] = velocityY[i-1] + (Y[i])*0.001;
+  velocityCurentY = velocityY[i];
+  velocityBeforeY = velocityY[i-1];
+  
+  velocityZ[i] = velocityZ + (Z[i])*0.001;
+  velocityCurentZ = velocityZ[i];
+  velocityBeforeZ = velocityZ[i-1];
 }
 
-plot(timeHorizon, vx)
-lines(timeHorizon,vx)
+plot(timeHorizon, velocityX, col = "black")
+lines(timeHorizon,velocityY, col = "blue")
+lines(timeHorizon, velocityZ, col = "red")
+title(main = "gráfico de Velocidade", xlab = "time", ylab = "Velocity", line = NA)
